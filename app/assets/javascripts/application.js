56f29aa2ef7fac2f609d50770ecd9a43eb491c9f
Akshay Shirole <akshay@redbytes.in>
2018-02-09 18:04:08 +0530

    sliders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,6 +34,33 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   });
 
+   $('.tab_img').lightSlider({
+              item:4,
+              loop:false,
+              slideMove:2,
+              easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+              speed:600,
+              responsive : [
+                  {
+                      breakpoint:800,
+                      settings: {
+                          item:3,
+                          slideMove:1,
+                          slideMargin:6,
+                        }
+                  },
+                  {
+                      breakpoint:480,
+                      settings: {
+                          item:2,
+                          slideMove:1
+                        }
+                  }
+              ]
+          });  
+
+
+
       // $(".intro-header").vegas({
 	// delay: 5000,
     // slides: [
